f0f090512296ac2d316f60600445898e73c55558
Bruce Tate <bruce@rapidred.com>
2019-02-25 17:01:59 -0500

    lib/mastermind otp implementation

--- lab4/mastermind/lib/boundary/game_server.ex
+++ lab4/mastermind/lib/boundary/game_server.ex
@@ -1,26 +1,32 @@
 defmodule Mastermind.Boundary.GameServer do
-  use GenServer
-  alias Mastermind.Core.Game
-  
-  # Client
-      
+	  use GenServer
+	  alias Mastermind.Core.Game
+	
+	  # Client
+	
     def start_link(answer) when is_list(answer) do
+      GenServer.start_link(__MODULE__, Game.new(answer))
     end
-  
-    def move(_pid, _answer) do
+
+    def move(pid, answer) do
+      GenServer.call(pid, {:move, answer})
     end
-  
-    def state(_pid) do
+
+    def state(pid) do
+      GenServer.call(pid, :state)
     end
-  
+
     # Server (callbacks)
-  
-    def init(_game) do
+
+    def init(game) do
+      {:ok, game}
     end
-  
-    def handle_call({:move, _answer}, _from, _game) do
+
+    def handle_call({:move, answer}, _from, game) do
+      {:reply, :ok, Game.move(game, answer)}
     end
-  
-    def handle_call(:state, _from, _game) do
+
+    def handle_call(:state, _from, game) do
+      {:reply, Game.state(game), game }
     end
-end
+end 
--- lab4/mastermind/lib/mastermind.ex
+++ lab4/mastermind/lib/mastermind.ex
@@ -2,11 +2,14 @@ defmodule Mastermind do
   alias Mastermind.Boundary.GameServer
   
   def new(answer \\ nil) do
+    {:ok, pid} = GameServer.start_link(answer)
   end
-  
-  def state(game) do
+
+  def state(pid) do
+    GameServer.state(pid)
   end
-  
-  def move(game, answer) do
+
+  def move(pid, answer) do
+    GameServer.move(pid, answer)
   end
 end
